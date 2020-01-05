038afd7cb02f2d71a8fe4665fcd18f96d38c6c72
hyeokjin-eun <gurwls2399@gmail.com>
2020-01-05 14:26:21 +0900

    튜플 학습

--- section04-3.py
+++ section04-3.py
@@ -58,5 +58,31 @@ print(y)
 y.pop
 print(y)
 ex = [88, 77]
+# y.append(ex)
 y.extend(ex)
 print(y)
+
+
+# 튜플 (순서 O, 중복 O, 수정 X, 삭제 X)
+a = ()
+b = (1,)
+c = (1, 2, 3, 4)
+d = (10, 100, ('a', 'b', 'b'))
+
+print(c[2])
+print(c[3])
+print(d[2][1])
+
+print(d[2:])
+print(d[2][0:2])
+
+print(c + d)
+print(c * 3)
+
+
+# 튜플 함수
+z = (5, 2, 1, 3, 4)
+print(z)
+print(3 in z)
+print(z.index(5))
+print(z.count(1))
