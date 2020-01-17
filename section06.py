6169d0d3d53c8f26c306916659241dc6950f4528
hyeokjin-eun <gurwls2399@gmail.com>
2020-01-17 13:37:48 +0900

    함수 및 람다
    함수 및 람다 3 학습

--- section06.py
+++ section06.py
@@ -91,3 +91,37 @@ def nested_func(num):
     func_in_func(num + 1000)
 
 nested_func(1000)
+
+# ex6 (힌트 (return 값 및 param 알려주는것))
+def func_mul3(x : int) -> list:
+    y1 = x * 100
+    y2 = x * 200
+    y3 = x * 300
+    return [y1, y2, y3]
+
+print(func_mul3(5))
+
+# 람다식 예제
+# 람다식 : 메모리 절약, 가독성 향상, 코드 간결
+# 함수는 객체 생성 -> 리소스(메모리) 할당
+# 람다는 즉시 실행(Heep 초기화) -> 메모리 초기화
+
+# 일반적 함수 -> 변수 할당
+def mul_10(num : int) -> int :
+    return num * 10
+
+var_func = mul_10
+print(type(var_func), var_func)
+print(var_func(10))
+
+# 람다식
+lambda_mul_10 = lambda num: num * 10
+
+print(lambda_mul_10(10))
+
+def func_final(x, y, func):
+    print(x * y * func(10))
+
+
+func_final(10, 10, lambda_mul_10)
+print(func_final(10, 10, lambda x: x * 1000))
