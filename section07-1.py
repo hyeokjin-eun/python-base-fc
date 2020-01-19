fd7cb34e1032a6761065e989e3e2cc08a372bf2d
hyeokjin-eun <gurwls2399@gmail.com>
2020-01-19 22:26:22 +0900

    클래스 Self, 클래스 변수, 인스턴스
    강의 학습 마무리

--- section07-1.py
+++ section07-1.py
@@ -2,6 +2,11 @@
 # 파이썬 클래스 상세 이해
 # self, class, instense 변수
 
+# 클래스, 인스턴스 차이 중요
+# 네임스페이스 : 객체를 인스턴스화 할 때 저장된 공간
+# 클래스 변수 : 직접사용 가능, 객체보다 먼저 생성
+# 인스턴스 변수 : 객체마다 별도로 존재, 인스턴스 생성후 사용
+
 # 선언
 class ClassCreate:
     pass
@@ -29,3 +34,57 @@ print(id(user2))
 
 print(user1.__dict__)
 print(user2.__dict__)
+
+# ex2 
+# self 의 이해
+
+class SelfTest() :
+    def function1():
+        print("Function 1 Called")
+    def function2(self):
+        print(id(self))
+        print("Function 2 Called")
+
+self_test = SelfTest()
+# self_test.function1() X
+SelfTest.function1()
+self_test.function2()
+
+print(id(self_test))
+# SelfTest.function2() X
+SelfTest.function2(self_test)
+
+# ex3
+# 클래스 변수, 인스턴스 변수
+
+class WareHouse:
+    # 클래스 변수
+    stock_num = 0
+    def __init__(self, name):
+        self.name = name
+        WareHouse.stock_num += 1
+    def __del__(self):
+        WareHouse.stock_num -= 1
+
+user1 = WareHouse("kim")
+user2 = WareHouse("pack")
+user3 = WareHouse("lee")
+
+print(user1.__dict__)
+print(user2.__dict__)
+print(user3.__dict__)
+
+print(WareHouse.__dict__) # 클래스 네임스페이스, 클래스 변수(공유)
+
+print(user1.name)
+print(user2.name)
+print(user3.name)
+
+print(user1.stock_num)
+print(user2.stock_num)
+print(user3.stock_num)
+
+del user1
+
+print(user2.stock_num)
+print(user3.stock_num)
